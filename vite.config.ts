bbcb471743303247013a5c3b4f2b73ce19f023da
Yojan Shakya <yojen.shakya@gmail.com>
2023-05-13 14:34:30 +0545

    test: test setup completed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  envPrefix: "APP_"
+export default defineConfig(async () => {
+  return {
+    plugins: [react()],
+    envPrefix: "APP_",
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["./src/config/setupTests.ts"],
+      coverage: {
+        reporter: ["text","html"],
+        provider: "c8"
+      }
+    },
+  }
 })
